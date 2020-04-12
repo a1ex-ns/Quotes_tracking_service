1955a24345213ab70fa13036ac0c1383933ce340
a1ex_ns <a1exsavchenkowm@gmail.com>
2020-04-12 16:07:20 +0500

    Refactoring hashCode and equals

--- src/main/java/quotes/tracking/model/EnergyLevel.java
+++ src/main/java/quotes/tracking/model/EnergyLevel.java
@@ -50,35 +50,29 @@ public class EnergyLevel {
     public void setElvl(Double elvl) {
     	this.elvl = elvl;
     }
-    
+
     @Override
     public int hashCode() {
-    	final int prime = 31;
-    	int result = 1;
-    	result = prime * result + ((elvl == null) ? 0 : elvl.hashCode());
-    	result = prime * result + ((quote == null) ? 0 : quote.hashCode());
-    	return result;
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((elvl == null) ? 0 : elvl.hashCode());
+        return result;
     }
-    
+
     @Override
     public boolean equals(Object obj) {
-    	if (this == obj)
-    		return true;
-    	if (obj == null)
-    		return false;
-    	if (getClass() != obj.getClass())
-    		return false;
-    	EnergyLevel other = (EnergyLevel) obj;
-    	if (elvl == null) {
-    		if (other.elvl != null)
-    			return false;
-    	} else if (!elvl.equals(other.elvl))
-    		return false;
-    	if (quote == null) {
-    		if (other.quote != null)
-    			return false;
-    	} else if (!quote.equals(other.quote))
-    		return false;
-    	return true;
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        EnergyLevel other = (EnergyLevel) obj;
+        if (elvl == null) {
+            if (other.elvl != null)
+                return false;
+        } else if (!elvl.equals(other.elvl))
+            return false;
+        return true;
     }
 }
